8e8c8002a802c0847c441b1d23ec0a956effbe5f
MohammadHroubSCME <22310018@scme.edu.ps>
2024-12-04 09:56:37 +0200

    محاضرة الاربعاء 4/12

---
+++ python/creat dic.py
@@ -0,0 +1,5 @@
+dictionary = {"karim":22210145, "Mohammad":22210198, "Mos":22210233, "naser":22210155} # Create a dictionary
+dictionary["susan"] = 50
+del dictionary["Mohammad"] # Delete a key-value pair
+print ('john' in dictionary) # Check if a key exists
+print(dictionary) # Print the dictionary
