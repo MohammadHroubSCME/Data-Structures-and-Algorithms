dd691d989a673d0badb681bae5b96d8d17354d87
MohammadHroubSCME <22310018@scme.edu.ps>
2024-12-10 09:36:15 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,3 +1,21 @@
 def no():
     from linkedlst import linkedlist
     
+    lst = linkedlist()
+    lst.add("Hebron")
+    print (lst)
+
+    lst.add("Ramallah")
+    print (lst)
+
+    lst.add("Nablus")
+    print (lst)
+
+    lst.add("Bethlehem")
+    print (lst)
+
+    lst.add("Jenin")
+    print (lst)
+
+    lst.add("Tolkarem")
+    print (lst)
